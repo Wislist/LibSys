d2cbf5c998543cdb9ce8f74bca570096e64fe8ea
Wislist <2929250563@qq.com>
2024-06-03 13:37:39 +0800

    Revert "键盘输入enter可以进行登录操作 添加新test"
    This reverts commit 1356be6a5d87cf680cf9dcaa178ff7173ce35938.

--- src/main/java/Controller/LoginHandler.java
+++ src/main/java/Controller/LoginHandler.java
@@ -1,62 +1,24 @@
 package Controller;
 
-import Pojo.Users;
 import Service.UserService;
 import Views.Login;
 
-import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 
-public class LoginHandler extends KeyAdapter implements ActionListener {
+public class LoginHandler implements ActionListener {
 
-   private Login login;
+   private Login loginView;
 
    //引入Service层
-
-
-    private Users user;
+    private UserService userService;
 
     public LoginHandler(Login login) {
-        this.login =login;
+        this.loginView =login;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        JButton jButton = (JButton) e.getSource();
-        String text = jButton.getText();
-        if ("登录".equals(text)){
-            login();
-        } else if ("注册".equals(text)) {
-
-        }
-
-
-    }
-
-    private void login() {
-        //获取到内容
-        String name = login.getUserTextField().getText();
-        String password = login.getPassWordTextField().getText();
-//            System.out.println(name+" "+password);
-        /**
-         * 查询数据库
-         */
-        boolean flag = false;
-        if (flag){
-
-        }else {
-            JOptionPane.showConfirmDialog(login,"用户名或者密码错误！");
-        }
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        if (KeyEvent.VK_ENTER == e.getKeyCode()){
-            login();
-        }
 
     }
 }
