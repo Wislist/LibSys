94712fe5b71eb463bbc4bf73d50c6d4347d649a3
Brondo-R <2134419075@qq.com>
2024-06-12 16:34:01 +0800

    修改了学生主页面

--- src/main/java/Views/StudentInterface/StuMainInterface/StuMainInterface.java
+++ src/main/java/Views/StudentInterface/StuMainInterface/StuMainInterface.java
@@ -4,6 +4,7 @@
 
 package Views.StudentInterface.StuMainInterface;
 
+import Pojo.StudentLogin;
 import Views.LoginAndRegisterView.LoginView;
 import Views.StudentInterface.AlterPanel.AlterPanel;
 import Views.StudentInterface.StuInformation.StuInformation;
@@ -71,18 +72,26 @@ public class StuMainInterface extends JFrame {
             }
         });
 
+        clientbutton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+//                client client=new client();
+            }
+        });
+
     }
     private void initComponents() {
 
         // JFormDesigner - Component initialization - DO NOT MODIFY
         ResourceBundle bundle = ResourceBundle.getBundle("student");
         panel1 = new JPanel();
-        panel2 = new JPanel();
+
         panel3 = new JPanel();
         button1 = new JButton();
         checkbutton = new JButton();
         alertbutton = new JButton();
         applybutton = new JButton();
+        clientbutton= new JButton();
 
         //======== this ========
         setTitle(bundle.getString("StuMainInterface.this.title"));
@@ -93,28 +102,7 @@ public class StuMainInterface extends JFrame {
         {
             panel1.setLayout(null);
 
-            //======== panel2 ========
-            {
-                panel2.setBorder(new EtchedBorder());
-                panel2.setLayout(null);
 
-                {
-                    // compute preferred size
-                    Dimension preferredSize = new Dimension();
-                    for(int i = 0; i < panel2.getComponentCount(); i++) {
-                        Rectangle bounds = panel2.getComponent(i).getBounds();
-                        preferredSize.width = Math.max(bounds.x + bounds.width, preferredSize.width);
-                        preferredSize.height = Math.max(bounds.y + bounds.height, preferredSize.height);
-                    }
-                    Insets insets = panel2.getInsets();
-                    preferredSize.width += insets.right;
-                    preferredSize.height += insets.bottom;
-                    panel2.setMinimumSize(preferredSize);
-                    panel2.setPreferredSize(preferredSize);
-                }
-            }
-            panel1.add(panel2);
-            panel2.setBounds(155, 30, 600, 400);
 
             //======== panel3 ========
             {
@@ -135,9 +123,13 @@ public class StuMainInterface extends JFrame {
                 //---- applybutton ----
                 applybutton.setText(bundle.getString("StuMainInterface.applybutton.text"));
                 panel3.add(applybutton);
+
+                //---- clientbutton ----
+                clientbutton.setText(bundle.getString("StuMainInterface.clientbutton.text"));
+                panel3.add(clientbutton);
             }
             panel1.add(panel3);
-            panel3.setBounds(30, 30, 110, 390);
+            panel3.setBounds(30, 30, 580, 100);
 
             {
                 // compute preferred size
@@ -164,20 +156,26 @@ public class StuMainInterface extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-//    public static void main(String[] args) {
-////        Student student=new Student
-////                ("张三", "123456", "男","2023001", 20,"13812345678" ,"计算机科学", 101, 1);
-//        Student student=new Student("张三", "123456","2023001");
-//        new StuMainInterface(student).setVisible(true);
-//    }
+    public static void main(String[] args) {
+    //    Student student=new Student
+      //          ("张三",
+        //                "123456", "男","2023001", 20,"13812345678" ,"计算机科学", 101, 1);
+        StudentLogin studentLogin = new StudentLogin();
+        String name = studentLogin.getName();
+        String psw = studentLogin.getPassword();
+        String id = studentLogin.getStuID();
+        Student student=new Student(name, psw,id);
+        new StuMainInterface(student).setVisible(true);
+    }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
     private JPanel panel1;
-    private JPanel panel2;
+
     private JPanel panel3;
     private JButton button1;
     private JButton checkbutton;
     private JButton alertbutton;
     private JButton applybutton;
+    private JButton clientbutton;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 }
